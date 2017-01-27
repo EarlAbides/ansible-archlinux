bce1e2600510ac5ed236a3c43ef1fbed26b85947
Jeffrey Earl <jeffreyearl@hotmail.com>
2017-01-26 21:18:44 -0500

    Add Vagrant base box guide

--- run-root.sh
+++ run-root.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ansible-playbook --connection=local arch-root.yml
--- run-user.sh
+++ run-user.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ansible-playbook --connection=local --ask-sudo-pass arch-user.yml
